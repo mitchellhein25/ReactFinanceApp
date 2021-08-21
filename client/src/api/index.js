1c3838ee918a9e86b6a080018f6a4bfea1ff1cbd
mitchellhein25 <mitchelldalehein25@gmail.com>
2021-08-20 19:26:11 -0500

    restructure for deployment

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+var baseUrl = process.env.NODE_ENV === 'production' ? 'https://murmuring-coast-12450.herokuapp.com/' : 'http://localhost:5000';
+
+const API = axios.create({ baseURL: baseUrl });
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
