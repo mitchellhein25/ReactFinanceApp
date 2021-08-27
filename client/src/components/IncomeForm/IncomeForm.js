5de5247ac959e9b87bfaf5bc2868e745784cde6d
mitchellhein25 <mitchelldalehein25@gmail.com>
2021-08-27 06:25:23 -0500

    Started adding front-end income form validation

--- client/src/components/IncomeForm/IncomeForm.js
+++ client/src/components/IncomeForm/IncomeForm.js
@@ -11,6 +11,7 @@ const IncomeForm = ({ currentId, setCurrentId }) => {
     const [incomeData, setIncomeData] = useState({
         date: moment(Date.now()).format("yyyy-MM-DD"), category: '', amount: '', description: ''
     });
+    const [errors, setErrors] = useState({});
     const income = useSelector((state) => currentId ? state.incomes.find((x) => x._id === currentId) : null);
     const dispatch = useDispatch();
     const classes = useStyles();
@@ -27,6 +28,45 @@ const IncomeForm = ({ currentId, setCurrentId }) => {
         format = format || "mm/yyyy";
         return format.toLowerCase()
     });
+    const validate = () => {
+        let temp = { ...errors }
+    
+        if ("date" in incomeData)
+          temp.date = incomeData.date ? "" : "Income date is required."
+    
+        if ("category" in incomeData)
+            temp.category = incomeData.category ? "" : "Income category is required."
+
+        if ("amount" in incomeData)
+            temp.amount = /^\d*(\.\d{1,2})?$/.test(incomeData.amount)
+            ? ""
+            : "Amount is not valid."
+
+        formIsValid();  
+        setErrors({
+          ...temp
+        });
+      }
+
+    const handleInputValue = (e) => {
+        const { name, value } = e.target;
+        if ([name] == 'category')
+            findIncomeCatId(e)
+        else {
+            setIncomeData({ ...incomeData, [name]: value });
+        }
+        validate();  
+    }
+
+    const formIsValid = () => {
+        const isValid =
+            incomeData.date &&
+            incomeData.amount &&
+            incomeData.category &&
+            Object.values(errors).every((x) => x === "");
+    
+        return isValid;
+    };
 
     useEffect(() => {
         if(income) {
@@ -65,31 +105,60 @@ const IncomeForm = ({ currentId, setCurrentId }) => {
         <Container className={classes.paper} style={isMobile ? {paddingLeft: '.5rem'} : {paddingLeft: '2rem'}}>
             <form className={`${classes.root} ${classes.form}`} autoComplete="off" noValidate onSubmit={handleSubmit}>
                 <Typography variant="h6">{ currentId ? 'Editing' : 'Enter' } an Income</Typography>
-                <TextField size="small" name="date" variant="outlined" type="date" fullWidth value={incomeData.date}
-                onChange={(e) => setIncomeData({ ...incomeData, date: e.target.value })}
+                <TextField 
+                    size="small" 
+                    name="date" 
+                    variant="outlined" 
+                    type="date" 
+                    fullWidth 
+                    value={incomeData.date}
+                    onChange={(e) => setIncomeData({ ...incomeData, date: e.target.value })}
                 />
                 <FormControl size="small" fullWidth variant="outlined">
                     <InputLabel className={classes.inputMargin} id="categoryLabel">Category</InputLabel>
-                    <Select className={classes.inputMargin} labelId="categoryLabel" size="small" name="category" 
-                    variant="outlined" fullWidth value={category} onChange={findIncomeCatId}>
+                    <Select 
+                        className={classes.inputMargin} 
+                        labelId="categoryLabel" 
+                        size="small" 
+                        name="category" 
+                        variant="outlined" 
+                        fullWidth 
+                        value={category} 
+                        onChange={findIncomeCatId}
+                    >
                         {incomeCatsToRender}
                     </Select>
                 </FormControl>
                 <FormControl fullWidth className={classes.margin} variant="outlined">
                     <InputLabel className={classes.inputMargin} >Amount</InputLabel>
-                    <OutlinedInput className={classes.inputMargin} size="small" name="amount" 
-                    variant="outlined" type="number" label="Amount" fullWidth 
-                    startAdornment={<InputAdornment position="start">$</InputAdornment>} value={incomeData.amount}  onChange={(e) => setIncomeData({ ...incomeData, amount: e.target.value })} />
+                    <OutlinedInput 
+                        className={classes.inputMargin} 
+                        size="small" 
+                        name="amount" 
+                        variant="outlined" 
+                        type="number" 
+                        label="Amount" 
+                        fullWidth 
+                        startAdornment={<InputAdornment position="start">$</InputAdornment>} 
+                        value={incomeData.amount}  
+                        onChange={(e) => setIncomeData({ ...incomeData, amount: e.target.value })} />
                 </FormControl> 
                 <FormControl fullWidth className={classes.margin} variant="outlined">
                     <InputLabel className={classes.inputMargin} >Description</InputLabel>
-                    <OutlinedInput className={classes.inputMargin} size="small" name="amount" 
-                    variant="outlined" type="text" label="Description" fullWidth 
-                    value={incomeData.description}  onChange={(e) => setIncomeData({ ...incomeData, description: e.target.value })} />
+                    <OutlinedInput 
+                        className={classes.inputMargin} 
+                        size="small" 
+                        name="amount" 
+                        variant="outlined" 
+                        type="text" 
+                        label="Description" 
+                        fullWidth 
+                        value={incomeData.description}  
+                        onChange={(e) => setIncomeData({ ...incomeData, description: e.target.value })} />
                 </FormControl>   
                 <div className={classes.buttonRow} >
                     <div className={classes.formElement} >
-                        <Button variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button> 
+                        <Button variant="contained" color="primary" size="large" type="submit" fullWidth disabled={!formIsValid()}>Submit</Button> 
                     </div>
                     <div className={classes.formElement} >
                         <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button> 
